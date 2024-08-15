5d0b5007a03f9dcf857c584fdc026a565a4d04d5
Shane Celis <shane.celis@gmail.com>
2024-08-15 05:08:22 -0400

    feature: Accept a script name.

--- src/bin/nano-9.rs
+++ src/bin/nano-9.rs
@@ -1,30 +1,21 @@
 use bevy::prelude::*;
 use bevy_mod_scripting::prelude::*;
 use nano_9::*;
+use std::env;
 
 fn main() -> std::io::Result<()> {
+    let mut args = env::args();
+    let script_path: String = args.next().unwrap_or("scripts/main.lua".into());
     App::new()
         .add_plugins(Nano9Plugin::default())
-        .add_systems(Startup, setup)
+        .add_systems(Startup, move |asset_server: Res<AssetServer>, mut commands: Commands| {
+            commands.spawn(ScriptCollection::<LuaFile> {
+                scripts: vec![Script::new(
+                    script_path.clone(),
+                    asset_server.load(&script_path),
+                )],
+            });
+        })
         .run();
-
     Ok(())
-}
-
-fn setup(asset_server: Res<AssetServer>, mut commands: Commands,
-         mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
-) {
-    let script_path = "scripts/basic.lua";
-    // let image = asset_server.load("images/oh no more goblins.png");
-    // let layout = TextureAtlasLayout::from_grid(Vec2::splat(12.0), 24, 24, None, None);
-    // let image = asset_server.load("images/Cat_Sprite.png");
-    // let layout = TextureAtlasLayout::from_grid(Vec2::splat(32.0), 4, 8, None, None);
-    // commands.insert_resource(Nano9SpriteSheet(image, texture_atlas_layouts.add(layout)));
-
-    commands.spawn(ScriptCollection::<LuaFile> {
-        scripts: vec![Script::new(
-            script_path.to_owned(),
-            asset_server.load(script_path),
-        )],
-    });
 }
