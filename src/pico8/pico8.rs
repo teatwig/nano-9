d9575685127dfee59008cc95062729a05b05f002
Shane Celis <shane.celis@gmail.com>
2025-01-26 01:17:23 -0500

    feature: Add oval*().

--- src/pico8/pico8.rs
+++ src/pico8/pico8.rs
@@ -886,6 +886,141 @@ impl Pico8<'_, '_> {
             .id();
         Ok(id)
     }
+
+    fn ovalfill(
+        &mut self,
+        upper_left: IVec2,
+        lower_right: IVec2,
+        color: Option<N9Color>,
+    ) -> Result<Entity, Error> {
+        let color = self.get_color(color.unwrap_or(N9Color::Pen))?;
+        // let min = a.min(b);
+        let size: UVec2 = ((lower_right - upper_left) + IVec2::ONE).try_into().unwrap();
+        // // let size = UVec2::new((a.x - b.x).abs() + 1,
+        // //                       (a.y - b.y).abs() + 1);
+        // let size = UVec2::new(delta.x.abs() as u32, delta.y.abs() as u32) + UVec2::ONE;
+        // dbg!(a, b, size);
+        let mut pixmap = Pixmap::new(size.x, size.y).expect("pixmap");
+        let oval =
+            tiny_skia::Rect::from_ltrb(0.0, 0.0, size.x as f32, size.y as f32).expect("oval rect");
+        let path = PathBuilder::from_oval(oval).expect("oval path");
+        let mut paint = Paint::default();
+        paint.anti_alias = false;
+        paint.set_color_rgba8(255, 255, 255, 255);
+        pixmap.fill_path(
+            &path,
+            &paint,
+            FillRule::Winding,
+            tiny_skia::Transform::identity(),
+            None,
+        );
+
+        let mut image = Image::new(
+            Extent3d {
+                width: size.x,
+                height: size.y,
+                depth_or_array_layers: 1,
+            },
+            TextureDimension::D2,
+            pixmap.take(),
+            TextureFormat::Rgba8UnormSrgb,
+            RenderAssetUsages::RENDER_WORLD,
+        );
+        let handle = self.images.add(image);
+        let clearable = Clearable::default();
+        let offset = 0.5;
+        let id = self
+            .commands
+            .spawn((
+                Name::new("ovalfill"),
+                Sprite {
+                    image: handle,
+                    color,
+                    anchor: Anchor::TopLeft,
+                    custom_size: Some(Vec2::new(size.x as f32, size.y as f32)),
+                    // image_mode: SpriteImageMode::Sliced(TextureSlicer {
+                    //     border: BorderRect::square(1.0),
+                    //     center_scale_mode: SliceScaleMode::Stretch,
+                    //     sides_scale_mode: SliceScaleMode::Tile { stretch_value: 1.0 },
+                    //     ..default()
+                    // }),
+                    ..default()
+                },
+                Transform::from_xyz(upper_left.x as f32, -(upper_left.y as f32), clearable.suggest_z()),
+                clearable,
+            ))
+            .id();
+        Ok(id)
+    }
+
+    fn oval(
+        &mut self,
+        upper_left: IVec2,
+        lower_right: IVec2,
+        color: Option<N9Color>,
+    ) -> Result<Entity, Error> {
+        let color = self.get_color(color.unwrap_or(N9Color::Pen))?;
+        // let min = a.min(b);
+        let size: UVec2 = ((lower_right - upper_left) + IVec2::ONE).try_into().unwrap();
+        // // let size = UVec2::new((a.x - b.x).abs() + 1,
+        // //                       (a.y - b.y).abs() + 1);
+        // let size = UVec2::new(delta.x.abs() as u32, delta.y.abs() as u32) + UVec2::ONE;
+        // dbg!(a, b, size);
+        let mut pixmap = Pixmap::new(size.x, size.y).expect("pixmap");
+        let oval =
+            tiny_skia::Rect::from_ltrb(0.0, 0.0, size.x as f32, size.y as f32).expect("oval rect");
+        let path = PathBuilder::from_oval(oval).expect("oval path");
+        let mut paint = Paint::default();
+        paint.anti_alias = false;
+        paint.set_color_rgba8(255, 255, 255, 255);
+        let mut stroke = Stroke::default();
+        stroke.width = 0.0;
+        pixmap.stroke_path(
+            &path,
+            &paint,
+            &stroke,
+            tiny_skia::Transform::identity(),
+            None,
+        );
+
+        let mut image = Image::new(
+            Extent3d {
+                width: size.x,
+                height: size.y,
+                depth_or_array_layers: 1,
+            },
+            TextureDimension::D2,
+            pixmap.take(),
+            TextureFormat::Rgba8UnormSrgb,
+            RenderAssetUsages::RENDER_WORLD | RenderAssetUsages::MAIN_WORLD,
+        );
+
+        let offset = 0.5;
+        let handle = self.images.add(image);
+        let clearable = Clearable::default();
+        let id = self
+            .commands
+            .spawn((
+                Name::new("oval"),
+                Sprite {
+                    image: handle,
+                    color,
+                    anchor: Anchor::TopLeft,
+                    custom_size: Some(Vec2::new(size.x as f32, size.y as f32)),
+                    // image_mode: SpriteImageMode::Sliced(TextureSlicer {
+                    //     border: BorderRect::square(1.0),
+                    //     center_scale_mode: SliceScaleMode::Stretch,
+                    //     sides_scale_mode: SliceScaleMode::Tile { stretch_value: 1.0 },
+                    //     ..default()
+                    // }),
+                    ..default()
+                },
+                Transform::from_xyz(upper_left.x as f32, -(upper_left.y as f32), clearable.suggest_z()),
+                clearable,
+            ))
+            .id();
+        Ok(id)
+    }
 }
 
 enum SfxDest {
@@ -1378,6 +1513,42 @@ fn attach_api(app: &mut App) {
                     pico8.circ(
                         IVec2::new(x0.unwrap_or(0), y0.unwrap_or(0)),
                         UVec2::splat(r.unwrap_or(4)),
+                        c,
+                    )
+                })?;
+                Ok(())
+            },
+        )
+        .register(
+            "ovalfill",
+            |ctx: FunctionCallContext,
+             x0: Option<i32>,
+             y0: Option<i32>,
+             x1: Option<i32>,
+             y1: Option<i32>,
+             c: Option<N9Color>| {
+                let _ = with_pico8(&ctx, move |pico8| {
+                    pico8.ovalfill(
+                        IVec2::new(x0.unwrap_or(0), y0.unwrap_or(0)),
+                        IVec2::new(x1.unwrap_or(0), y1.unwrap_or(0)),
+                        c,
+                    )
+                })?;
+                Ok(())
+            },
+        )
+        .register(
+            "oval",
+            |ctx: FunctionCallContext,
+             x0: Option<i32>,
+             y0: Option<i32>,
+             x1: Option<i32>,
+             y1: Option<i32>,
+             c: Option<N9Color>| {
+                let _ = with_pico8(&ctx, move |pico8| {
+                    pico8.oval(
+                        IVec2::new(x0.unwrap_or(0), y0.unwrap_or(0)),
+                        IVec2::new(x1.unwrap_or(0), y1.unwrap_or(0)),
                         c,
                     )
                 })?;
