b830a8592be20cd064b8f500bc4cead842007a73
Shane Celis <shane.celis@gmail.com>
2025-05-09 06:53:07 -0400

    test: Add png read test.

--- tests/gfx-test.rs
+++ tests/gfx-test.rs
@@ -36,6 +36,22 @@ fn test_read_indexed_png_indices_from_file_2bit() {
 }
 
 #[test]
+fn test_read_indexed_png_indices_from_file_2bit() {
+    let data = fs::read("tests/gfx-test.png")
+        .expect("Failed to read test PNG file");
+
+    let gfx = Gfx::<2, u8>::from_png(&data).unwrap();
+    assert_eq!(gfx.get(0,0), 0);
+    assert_eq!(gfx.get(1,0), 1);
+    assert_eq!(gfx.get(0,1), 2);
+    assert_eq!(gfx.get(1,1), 1);
+    let v = gfx.data.into_vec();
+    assert_eq!(v.len(), 1);
+    // assert_eq!(v[0], 0b00_01_10_01);
+    assert_eq!(v[0], 0b01_10_01_00);
+}
+
+#[test]
 fn test_read_indexed_png_indices_from_file_big_pal() {
     let data = fs::read("tests/gfx-big-palette.png")
         .expect("Failed to read test PNG file");
