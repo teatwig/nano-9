e055ae6478fc47be862932eda20fbb1e9bc5a5da
tea <tea@teatwig.net>
2025-07-08 15:37:10 +0200

    feat: change font to PICO-8 wide

--- src/pico8/mod.rs
+++ src/pico8/mod.rs
@@ -31,12 +31,12 @@ pub(crate) use defaults::*;
 // mod gfx2;
 pub const PICO8_PALETTE: &str = "embedded://nano9/pico8/pico-8-palette.png";
 pub const PICO8_BORDER: &str = "embedded://nano9/pico8/rect-border.png";
-pub const PICO8_FONT: &str = "embedded://nano9/pico8/pico-8.ttf";
+pub const PICO8_FONT: &str = "embedded://nano9/pico8/pico-8-wide.ttf";
 
 pub(crate) fn plugin(app: &mut App) {
     embedded_asset!(app, "pico-8-palette.png");
     embedded_asset!(app, "rect-border.png");
-    embedded_asset!(app, "pico-8.ttf");
+    embedded_asset!(app, "pico-8-wide.ttf");
     app.add_plugins(api::plugin)
         .add_plugins(clear::plugin)
         .add_plugins(audio::plugin)
